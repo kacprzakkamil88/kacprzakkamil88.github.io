8b8ead5aafcf038aeadd8d6906f7d4e536a7439e
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-10 16:40:47 +0100

    zmienne

---
+++ warsztaty/java-script/zmienne/2-zmienne/2-var.js
@@ -0,0 +1,12 @@
+
+var imie="Johny"
+var nazwisko="Cash"
+var wiek = 44;
+var wzrost;
+
+//var wynik = "Imię: " + imie +   " Nazwisko: " + nazwisko + " Wiek: " + wiek;
+//console.log(imie);
+//console.log(nazwisko);
+//console.log(wiek);
+//console.log(wynik);
+
