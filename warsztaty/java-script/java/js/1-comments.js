8c6a00b2b1cd7d2e63dc2a54d7cef3925d2495e8
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-10 15:47:59 +0100

    komentarze

---
+++ warsztaty/java-script/java/js/1-comments.js
@@ -0,0 +1,7 @@
+/*
+Kommenatrz blokowy jest na wielu linijeczkach i robi się to co fajnego sktórem CTRL + SHIFT + / */
+
+
+// liniowy jest w jednej linijeczce i kończy się do końca tegóż wiersza a jego zacny skrót klawiszowy to CTRL + /
+to juz liniowy nie ma go... nie było go... gdzie on jest?
+
