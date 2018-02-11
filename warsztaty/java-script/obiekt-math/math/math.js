1e996f0a1990b3f9606b1201c29b624f343fe29c
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-11 13:51:36 +0100

    com

---
+++ warsztaty/java-script/obiekt-math/math/math.js
@@ -0,0 +1,22 @@
+//console.log(Math.E);
+
+//console.log(Math.PI);
+
+
+
+//console.log(Math.cos(29));
+
+//console.log(Math.pow(4,10)); //pierwiastek
+
+//console.log(Math.random());
+
+//console.log(Math.random());
+
+//console.log(Math.random());
+
+
+
+
+console.log (Math.ceil(1.75)); //zaokrgla w góre ---- często się używa
+console.log (Math.round(1.5)); // zaokrągla
+
