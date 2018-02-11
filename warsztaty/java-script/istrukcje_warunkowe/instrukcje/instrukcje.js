5e476a28c19cdd05e9f94a098ebb6feb88ae886e
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-11 15:22:33 +0100

    commyt

---
+++ warsztaty/java-script/istrukcje_warunkowe/instrukcje/instrukcje.js
@@ -0,0 +1,56 @@
+//var liczba1 = "smog";
+//var liczba2 = "smog";
+//var prawda = true;
+
+//var wynik = (liczba1 >= liczba2) ? "liczba1 większa od liczba2" : "liczba1 mniejsza od liczba2"; 
+
+
+//console.log(wynik);
+
+//var wynik (liczba1 == liczba2) ? "prawda" : "fałsz";
+//console.log(wynik);
+
+//var pietro=13;
+//if( liczba1 > liczba2 || pietro == 14 ) {
+  //  console.log("Sepłnione zostaly dwa warunki. także true");
+//}
+
+//wykrzyczczi np w !liczba1 to jest negacja tego obraca wynik końcowy
+
+
+//if (liczba1 == liczba2) {
+ //   console.log("liczby są równe");
+//} else if (liczba1 < liczba2) {
+//    console.log("liczba1 większa od liczby2");
+//} else if (liczba1 == "smog") {
+//    console.log("libcza równa się smog ");
+//} else {
+//    console.log("kod kóry wykona się gdy żaden nie będzie spełniony");
+//
+//}
+// --------------------------------- FUNKCJA SWITCH -------------------
+var grupa = 25;
+
+switch( grupa ) {
+    case 12 :
+        console.log("liczba osób w grupie = 12");
+    break; 
+        
+
+    case 14 :
+        console.log("liczba osób w grupie = 14");
+    break; 
+        
+    case 16 :
+        console.log("liczba osób w grupie = 14");
+    break; 
+    case 18 :
+        console.log("liczba osób w grupie = 18");
+    break; 
+    
+    case 20 :
+        console.log("liczba osób w grupie = 20");
+    break; 
+    default : 
+        console.log("Nie wiadomo ile jest osób w grupie")
+}
---
+++ warsztaty/java-script/operatory/operatory/operatory.js
@@ -0,0 +1,32 @@
+var liczba1 = 14;
+var liczba2 = 6;
+
+//console.log( liczba1 + liczba2 );
+
+//console.log( liczba1 / liczba2);
+
+//console.log( liczba1 - liczba2);
+
+//console.log(liczba1 % liczba2); // ----------------- modulo to znaczy jakieś dzielenie ------------- //
+
+//liczba1++;
+//console.log(liczba1);
+
+//liczba2--;
+//console.log(liczba2);
+
+//console.log(liczba1++); // jeśli plusy są z tyłu najpierw jest operacja a potem zmiana wartośc czyli 14 //////////////////////
+
+//console.log(++liczba1); // i w drugą stronę
+
+liczba1 += 2; // inny zapis liczba1 = liczba1 + 2
+console.log(liczba1);
+
+liczba1 -= 2;//
+console.log(liczba1);
+
+liczba1 *= 10;
+console.log(liczba1);
+
+liczba1 %= 6;
+console.log(liczba1);
