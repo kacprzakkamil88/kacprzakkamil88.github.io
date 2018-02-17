b0ac1b7deda4de322283156743962550528a85d9
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-17 16:46:11 +0100

    pierwszy spsob

---
+++ warsztaty/model-dom/warsztat/java/java.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+
+
+function ustawTlo () {
+   
+    var allParagraphs  = document.getElementsByTagName ("p")
+
+    allParagraphs[0].style.backgroundColor = "blue";
+    allParagraphs[1].style.backgroundColor = "red";
+    
+}
+
+//var input = document.getElementById("input-id");
+//input.addEventListener("click, u")
+
