dd26b095a8f0cf234e9024e81438efa0b4135200
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-18 11:38:54 +0100

    dom dom dom

--- warsztaty/model-dom/warsztat2/java/java.js
+++ warsztaty/model-dom/warsztat2/java/java.js
@@ -2,12 +2,38 @@
 
 // zablokować id///////
 
-var formularz = document.getElementById
+var formularz = document.getElementById("input");
 
-function pobranieImienia(event) {
+//function pobierzImie(event) {
+//    event.preventDefault();
+//    console.log("działa: ");
+//
+//    var form = document.getElementById("formularz");
+//    console.log(form);
+//    for (var i = 0; i < from.length; i++) {
+//        console.log(form.elements[i]).value);
+//    if (form.elements[i]).value != "Submit") {
+//    console.log(form.elements[i].value);
+//}
+//}
+//}
+
+
+// zadanie można zrobić na kilka sposób //////////// aha coś nie tak z nawiasami
+// powinno wyświetlać imię i nazwisko bez przeładowanie strony po 
+
+// inny sposób przez pobranie id // 
+function pobierzImie(event) {
     event.preventDefault();
-    console.log("działa: ");
-    
+    console.log("działa")
+
     var form = document.getElementById("formularz");
-    console.log (form);
+    console.log(form);
+    for (var i = 0; i < form.length; i++) {
+        console.log(form.elements[i].value);
+        if (form.elements[i].value != "Submit") {
+            console.log(form.elements[i].value);
+        }
+    }
+
 }
