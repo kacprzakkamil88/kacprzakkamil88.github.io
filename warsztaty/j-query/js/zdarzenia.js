0880fcc1fa44be1c81f153f23a4f6914cf6ae8a5
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-18 15:18:28 +0100

    dbl click

--- warsztaty/j-query/js/zdarzenia.js
+++ warsztaty/j-query/js/zdarzenia.js
@@ -1,8 +1,28 @@
 "use strict";
+//
+//$("#first").mouseenter(function() {
+//    $("#second").toggle(function() {
+//        $(this).css({"background-color": "yellow", "text-decoration": "underline"});
+//        
+//    });
+//});
 
-$("#first").mouseenter(function() {
-    $("#second").toggle(function() {
-        $(this).css({"background-color": "yellow", "text-decoration": "underline"});
-        
-    });
+// to to to to działa na dole to działa ---------------------------____________|||||||||||||||||||||||||||||||||||||
+$("#first").on({
+    "mouseover" : function () {
+        $(this).css("background-color", "yellow");
+    },
+    
+    "mouseleave" : function () {
+        $(this).css("background-color", "green");
+    },
+    
+    "dblclick" : function () {
+        console.log("click click");
+    }
+    
 });
+
+
+
+
