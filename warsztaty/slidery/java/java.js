60119d068586d75fa0114d87bbbf3bf043ff60b2
kacprzakkamil88 <kamilek.kacprzak88@gmail.com>
2018-02-24 12:56:12 +0100

    warszt dziala

--- warsztaty/slidery/java/java.js
+++ warsztaty/slidery/java/java.js
@@ -2,9 +2,62 @@
 
 var slideShow = $(".slide-show");
 
-var slideCount  = $(".single-slide");
+
+var slideCount  = $(".single-slide").length;
     
-var slideWidth = 100 / "slideCount";
+var slideWidth = 100 / slideCount;
 
 
-var slideIndex = $(".single-slide")[0];
+
+var slideIndex = 0;
+
+// jeżeli zdefinuje to w var to potem niepotrzebne jest jakieś pisanie doljarów ---------------------------------------
+
+
+
+slideShow.css("width", slideCount * 100 + "%");
+
+
+
+slideShow.find(".single-slide").each(function(index, element) {
+ $(this).css({
+     "width": slideWidth + "%",
+     "margin-left": slideWidth * index + "%",
+     
+     
+ })  
+    
+});
+
+
+$(".prev-slide").click(function() {
+    slide(slideIndex - 1);
+    
+});
+
+
+$(".next-slide").click(function() {
+    slide(slideIndex + 1);
+    
+});
+
+function slide(newSlideIndex) {
+    if(newSlideIndex < 0 || newSlideIndex > slideCount -1) {
+        return;
+}
+
+var slideCaption = $(".slide-caption").eq(newSlideIndex);                   // wartośc eq - 
+    
+    var marginLeft = newSlideIndex * (-100) + "%";
+    slideCaption.hide();
+    slideShow.animate ({
+        "margin-left": marginLeft, 
+    }, 800, function(){
+        slideIndex=newSlideIndex;
+        slideCaption.fadeIn();
+    });
+   
+    
+    };
+
+
